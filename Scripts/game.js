100bf3a1ef7b9b04edf0b22d194a8f57fb9cc447
Karolina Rybarczyk <szprota@rybki.com.pl>
2013-05-10 16:07:18 +0200

    game.js

--- Scripts/game.js
+++ Scripts/game.js
@@ -7,3 +7,25 @@ var minx = 0;
 var miny = 0;
 var maxx = 1218;
 var maxy = 664;
+
+
+
+function ruch
+{
+var klawisz = e.keyCode ? e.keyCode : e.which;
+
+switch(klawisz){
+case 37:
+//lewa strza³ka
+break;
+case 38:
+//strza³ka w górê
+break;
+case 39:
+//strza³ka w prawo
+break;
+case 40:
+//strza³ka w dó³
+break;
+}
+}
