e4b050f8a81371e54b294cac770c2635b060e191
Karolina Rybarczyk <szprota@rybki.com.pl>
2013-05-11 13:37:29 +0200

    game.js

--- Scripts/game.js
+++ Scripts/game.js
@@ -8,7 +8,31 @@ var miny = 0;
 var maxx = 1218;
 var maxy = 664;
 
+function onkeyup(e)
+{
+  var code = e.keyCode;
+  if (code == 37)
+    lewo = 0;
+  if (code == 38)
+    gora = 0;
+  if (code == 39)
+    prawo = 0;
+  if (code == 40)
+    dol = 0;
+}
 
+function onkeydown(e)
+{
+  var code = e.keyCode;
+  if (code == 37)
+    lewo = 1;
+  if (code == 38)
+    gora = 1;
+  if (code == 39)
+    prawo = 1;
+  if (code == 40)
+    dol = 1;
+}
 
 function ruch()
 {
@@ -81,4 +105,30 @@ if (klawisz==40){
 //Wykonywanie powyższej funkcji regularnie co określoną liczbę milisekund
 setTimeout("ruch()",50);
 
+}
+
+function onkeydown(e)
+{
+  var code = e.keyCode;
+  if (code == 37)
+    lewo = 1;
+  if (code == 38)
+    gora = 1;
+  if (code == 39)
+    prawo = 1;
+  if (code == 40)
+    dol = 1;
+}
+
+function onkeyup(e)
+{
+  var code = e.keyCode;
+  if (code == 37)
+    lewo = 0;
+  if (code == 38)
+    gora = 0;
+  if (code == 39)
+    prawo = 0;
+  if (code == 40)
+    dol = 0;
 }
